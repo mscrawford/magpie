be659d28442fa67862a98f4c88a83426f23b9054
Michael Crawford <mscrawfor@gmail.com>
2022-07-18 12:33:07 +0200

    Bugfix inms_reporting_reg.R

--- scripts/output/projects/inms_reporting_reg.R
+++ scripts/output/projects/inms_reporting_reg.R
@@ -18,49 +18,46 @@ library(magpie4)
 library(magpiesets)
 library(iamc)
 library(gms)
-print("Start inms reporting reg runscript")
+
+message("Start INMS regional reporting runscript")
 
 ############################# BASIC CONFIGURATION #######################################
 
-if(!exists("source_include")) {
+if (!exists("source_include")) {
 
-  title       <- "inms_SSP2_RCP4p5_PolicyLow_v4"
-  outputdir       <- "output/inms_SSP2_RCP4p5_PolicyLow_v4_2020-07-13_15.37.07"
+  title       <- NULL
+  outputdir   <- NULL
 
-  ###Define arguments that can be read from command line
-  readArgs("outputdir","title")
+  # Define arguments that can be read from command line
+  readArgs("outputdir", "title")
+
 }
+
 #########################################################################################
 
-print(paste0("script started for output directory",outputdir))
-
-wdbefore=getwd()
-on.exit(setwd(wdbefore))
-setwd(outputdir)
-
-cfg <- gms::loadConfig("config.yml")
+message("Script started for output directory: ", outputdir)
+cfg <- gms::loadConfig(file.path(outputdir, "config.yml"))
 title <- cfg$title
-print("generating INMS output for the run: ")
-print(title)
 
-filename=paste0("report_",title,".mif")
-gdx=paste0("fulldata.gdx")
-a=getReportINMS(gdx,file=filename,scenario = title,dir=".")
+message("Scenario title: ", title)
 
-print(filename)
-mif=read.report(filename)
+filename <- file.path(outputdir, paste0("report_", title, ".mif"))
+gdx <- file.path(outputdir, "fulldata.gdx")
 
+a <- getReportINMS(gdx, file = filename, scenario = title, dir = outputdir)
+mif <- read.report(filename)
 
-missingyears=function(x){
-  history=paste0("y",1965+((0:5)*5))
-  x[[1]][[1]] <- time_interpolate(x[[1]][[1]],interpolated_year = c(history,paste0("y",2005+((0:9)*10))),integrate_interpolated_years = TRUE)
-  x[[1]][[1]][,history,] = 0
+missingyears = function(x) {
+  history = paste0("y", 1965 + ((0:5) * 5))
+  x[[1]][[1]] <- time_interpolate(x[[1]][[1]],
+                                  interpolated_year = c(history, paste0("y", 2005 + ((0:9) * 10))),
+                                  integrate_interpolated_years = TRUE)
+  x[[1]][[1]][, history, ] = 0
   return(x)
 }
 
-#a=c(missingyears(ssp1),missingyears(ssp2))
-a=missingyears(mif)
+a = missingyears(mif)
 
-write.reportProject(a,mapping=paste0(wdbefore,"/mapping_inms.csv"),file="report_inms.mif")
-#write.report2(a,file="magpie_results_nov2019.mif")
+write.reportProject(a, mapping = "mapping_inms.csv", file = file.path(outputdir, "report_inms.mif"))
+
 warnings()
